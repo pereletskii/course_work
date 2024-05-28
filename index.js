2cf52c59217559a0c3fed672b4cb1590585d1936
Gleb Gorelov <qgorelov@gmail.com>
2024-05-28 17:50:15 +0300

    Random number genereator selection added

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ function standartDeviation(mu, data) {
     return standard_deviation
 }
 
-function normalDistributionLaw(data){
+function normalDistributionLaw(data, path){
     let hi = {
+        path: path,
         mu: 0,
         sigma: 0,
         mu_minus_sigma: 0,
@@ -68,19 +69,76 @@ function normalDistributionLaw(data){
     hi.e4 = hi.e1;
     hi.chi_2 = (hi.o1 - hi.e1) ** 2 / hi.e1 + (hi.o2 - hi.e2) ** 2 / hi.e2 + (hi.o3 - hi.e3) ** 2 / hi.e3 + (hi.o4 - hi.e4) ** 2 / hi.e4;
 
+    hi.chi_2_pr = chiSquarePValue(hi.chi_2, 1);
+
+    return hi
+}
+
+function uniformDistribution(data, path){
+    let hi = {
+        path: path,
+        mu: 0,
+        sigma: 0,
+        a: 0,
+        b: 0,
+        o1: 0,
+        o2: 0,
+        o3: 0,
+        o4: 0,
+        e1: 0,
+        e2: 0,
+        e3: 0,
+        e4: 0,
+        chi_2: 0,
+        chi_2_pr: 0,
+    }
+
+    hi.mu = sum(data) / data.length;
+    hi.sigma = standartDeviation(hi.mu, data);
+    hi.a = hi.mu - hi.sigma * Math.sqrt(3);
+    hi.b = hi.mu + hi.sigma * Math.sqrt(3);
     
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] <= hi.a) {
+            hi.o1++;
+        } else if (data[i] > hi.a && data[i] <= hi.mu) {
+            hi.o2++;
+        } else if (data[i] > hi.mu && data[i] <= hi.b) {
+            hi.o3++;
+        } else if (data[i] > hi.b) {
+            hi.o4++;
+        }
+    }
+
+    hi.e1 = 0.01 * data.length;
+    hi.e2 = 0.49 * data.length;
+    hi.e3 = hi.e2;
+    hi.e4 = hi.e1;
+    hi.chi_2 = (hi.o1 - hi.e1) ** 2 / hi.e1 + (hi.o2 - hi.e2) ** 2 / hi.e2 + (hi.o3 - hi.e3) ** 2 / hi.e3 + (hi.o4 - hi.e4) ** 2 / hi.e4;
 
     hi.chi_2_pr = chiSquarePValue(hi.chi_2, 1);
 
     return hi
 }
 
-function main(){
-    let out = []
+function main() {
+    let rng = { normal: [], uniform: [] };
+    let rngType = [];
+
     for ([key, value] of Object.entries(data)) {
-        out.push(normalDistributionLaw(data[key]).chi_2_pr);
+        rng.normal.push(normalDistributionLaw(data[key], key));
+        rng.uniform.push(uniformDistribution(data[key], key));
     }
-    console.log(out);
+    console.log(rng);
+
+    for (let i = 0; i < rng.normal.length; i++) {
+        if (rng.normal[i].chi_2_pr > rng.uniform[i].chi_2_pr) {
+            rngType.push('normal');
+        } else {
+            rngType.push('uniform');
+        }
+    }
+    console.log(rngType);
 }
 
 module.exports.main = main;
