daf754f11a4d8af3abb2061203087e6ff33d47b4
Gleb Gorelov <qgorelov@gmail.com>
2024-05-30 21:37:18 +0300

    Histogram appearance fixed

--- scripts/histogram.js
+++ scripts/histogram.js
@@ -1,10 +1,12 @@
 const { main, findRng } = require('./index.js');
+
+let n = 1000;
 function formData() {
-  let data = [];
+  let data = new Array(7).fill(0);
   let rng = findRng();
   
-  for (let i = 0; i < 1000; i++) {
-    data.push(main(rng).modelResult.vertice);
+  for (let i = 0; i < n; i++) {
+    data[main(rng).modelResult.vertice - 1]++;
   }
 
   return data
@@ -12,23 +14,15 @@ function formData() {
 
 function formHistogram() {
   let data = formData();
-
-  const counts = {};
-  for (const value of data) {
-    counts[value] = (counts[value] || 0) + 1;
-  }
-  const labels = Object.keys(counts);
-  const values = Object.values(counts);
-  const chartData = { labels, datasets: [{ label: 'Оптимальная точка', data: values, backgroundColor: 'rgba(124, 252, 0, 0.5)' }] };
+  const chartData = { labels: [1, 2, 3, 4, 5, 6, 7], datasets: [{ label: 'Количество вхождений', data: data, backgroundColor: 'rgba(124, 252, 0, 0.5)', minBarLength: 0 }] };
 
   const chartConfig = {
     type: 'bar',
     data: chartData,
     options: {
       scales: {
-        yAxes: [{ ticks: { beginAtZero: true } }]
-      },
-      width: '40%'
+        y: [{ ticks: { beginAtZero: true } }]
+      }
     }
   };
 
@@ -37,7 +31,8 @@ function formHistogram() {
   <script>\
       const ctx = document.getElementById('histogramChart').getContext('2d');\
       new Chart(ctx, ${JSON.stringify(chartConfig)});\
-  </script>`;
+  </script>\
+  <h2>Количество проверок: ${n}</h2>`;
 
   return html
 }
