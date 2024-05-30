3f47e98a65cf16bacd0f28463b609861444131bf
Gleb Gorelov <qgorelov@gmail.com>
2024-05-30 18:44:10 +0300

    Table names added

--- renderHTML.js
+++ renderHTML.js
@@ -21,7 +21,11 @@ function staticTablesFormer() {
     let headersNormal = {'path': 'Путь', 'mu': 'μ', 'sigma': 'σ', 'mu_minus_sigma': 'μ-σ', 'mu_plus_sigma': 'μ+σ', 'o1': 'o1', 'o2': 'o2', 'o3': 'o3', 'o4': 'o4', 'e1': 'e1', 'e2': 'e2', 'e3': 'e3', 'e4': 'e4', 'chi_2': 'χ²', 'chi_2_pr': 'χ²*'}
     var tableNormal = createTable(headersNormal, rng.normal);
 
-    let out = '<link rel="stylesheet" type="text/css" href="style.css" />' + tableNormal + tableUniform;
+    let out = '<link rel="stylesheet" type="text/css" href="style.css" />'
+        +'<h2 class="tableName">Хи-квадрат для нормального закона распределения</h2>'
+        +tableNormal
+        +'<h2 class="tableName">Хи-квадрат для равномерного закона распределения</h2>'
+        +tableUniform;
 
     return out
 }
@@ -34,6 +38,11 @@ let translate = {
     'path': 'Путь',
     'mu': 'μ',
     'sigma': 'σ',
+    'normalTable': 'Параметры генератора случайных чисел для нормального закона распределения',
+    'uniformTable': 'Параметры генератора случайных чисел для равномерного закона распределения',
+    'rngMatrix': 'Случайная взвешенная сеть',
+    'distancesMatrix': 'Матрица расстояний',
+    'radiuses': 'Радиусы'
 }
 
 function dynamicTablesFormer() {
@@ -51,10 +60,11 @@ function dynamicTablesFormer() {
                 headers[header] = header;
             }
         }
-        out += createTable(headers, table, 'smallTable');
+        let name = `<h2 class="tableName">${translate[tableName]}</h2>`;
+        out += name + createTable(headers, table, 'smallTable');
     }
 
-    let result = `<h2>Подходящая вершина: ${modelResult.vertice}</h2><h2>Внешне-внутренний радиус: ${modelResult.value}</h2>`;
+    let result = `<h2>Подходящая точка: ${modelResult.vertice}</h2><h2>Внешне-внутренний радиус: ${modelResult.value}</h2>`;
 
     return out + result
 }
