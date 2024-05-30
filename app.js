9c76bfc012eff620b0d68fefda029dd97ce8bb4f
Gleb Gorelov <qgorelov@gmail.com>
2024-05-30 16:13:56 +0300

    Tables formatter for better HTML render added

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 const express = require("express");
 const app = express();
+const Table = require('table-builder');
 
-const { main, findRng } = require('./scripts/index.js');
+const { findRng } = require('./scripts/index.js');
+const { formatTables } = require('./scripts/tablesHandler.js');
 
-function html() {
-    // TODO сделать генерацию html страницы
+function createTable(headers, data) {
+    return (
+        new Table({'class': 'table'})
+            .setHeaders(headers)
+            .setData(data)
+            .render()
+    )
 }
+
+function staticTablesFormer() {
+    let headersUniform = {'path': 'Путь', 'mu': 'μ', 'sigma': 'σ', 'a': 'a', 'b': 'b', 'o1': 'o1', 'o2': 'o2', 'o3': 'o3', 'o4': 'o4', 'e1': 'e1', 'e2': 'e2', 'e3': 'e3', 'e4': 'e4', 'chi_2': 'χ²', 'chi_2_pr': 'χ²*'};
+    var tableUniform = createTable(headersUniform, rng.uniform);
+
+    let headersNormal = {'path': 'Путь', 'mu': 'μ', 'sigma': 'σ', 'mu_minus_sigma': 'μ-σ', 'mu_plus_sigma': 'μ+σ', 'o1': 'o1', 'o2': 'o2', 'o3': 'o3', 'o4': 'o4', 'e1': 'e1', 'e2': 'e2', 'e3': 'e3', 'e4': 'e4', 'chi_2': 'χ²', 'chi_2_pr': 'χ²*'}
+    var tableNormal = createTable(headersNormal, rng.normal);
+
+    let out = '<link rel="stylesheet" type="text/css" href="style.css" />' + tableNormal + tableUniform + '<button onclick="window.location.reload()">Обновить</button>';
+
+    return out
+}
+
+function dynamicTablesFormer() {
+    let data = formatTables(rng)
+
+    for ([key, value] of Object.entries(data)) {
+        // TODO сделать формирование динамических таблиц в HTML
+    }
+}
+
+let rng = findRng();
+let staticTables = staticTablesFormer();
+
+app.use(express.static("public"));
 
 app.get("/", function(request, response){
     console.log(request.path);
-    response.send(html());
+
+    let dynamicTables = dynamicTablesFormer(rng);
+
+    response.send(staticTables + dynamicTables);
 });
 
 app.listen(3010);
---
+++ scripts/tablesHandler.js
@@ -0,0 +1,63 @@
+const { main, findRng } = require('./index.js');
+
+function JSONify(table){
+    let JSONrngMatrix = {}
+    for (let i = 0; i < table.length; i++) {
+        table[i] = [i + 1].concat(table[i]);
+        JSONrngMatrix[i + 1] = table[i];
+    }
+    return JSONrngMatrix
+}
+
+function formatTables(rng) {
+    let data = main(rng);
+    // console.log(data);
+
+    // console.log(data.rngType);
+    data['normalTables'] = {};
+    data['uniformTables'] = {};
+    for ([key, value] of Object.entries(data.rngType)) {
+        if (value.type == 'normal') {
+            data.normalTables[key] = value.params;
+        } else {
+            data.uniformTables[key] = value.params;
+        }
+    }
+    delete data['rngType'];
+    // console.log(data.normalTables);
+    // console.log(data.uniformTables);
+    // OK
+
+    // console.log(data.rngMatrix);
+    for (let i = 0; i < data.rngMatrix.length; i++) {
+        for (let j = 0; j < data.rngMatrix.length; j++) {
+            if (data.rngMatrix[i][j] == Infinity){
+                data.rngMatrix[i][j] = 0;
+            }
+            data.rngMatrix[j][i] = data.rngMatrix[i][j]
+        }   
+    }
+    // console.table(data.rngMatrix);
+
+    data.rngMatrix = JSONify(data.rngMatrix);
+    // console.table(data.rngMatrix);
+    // OK
+
+    // console.table(data.distancesMatrix);
+    data.distancesMatrix = JSONify(data.distancesMatrix);
+    // console.table(data.distancesMatrix);
+    // OK
+
+    // console.table(data.radiuses);
+    // OK
+
+    // console.table(data.modelResult);
+    // OK
+
+    return data
+}
+
+module.exports.formatTables = formatTables;
+
+let data = formatTables(findRng());
+console.log(data);
